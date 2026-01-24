18a3500d4fc773113619ab69b76d7c591289d9f3
Maxim Kleverov <mklevero@student.hive.fi>
2026-01-24 19:05:15 +0200

    more tests

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -4,6 +4,26 @@
 int main()
 {
 
+Animal animal;
+Cat	cat;
+Dog dog;
+
+Animal wild_cat = cat;
+Animal wild_dog(dog);
+
+animal.makeSound();
+wild_cat.makeSound();
+wild_dog.makeSound();
+cat.makeSound();
+dog.makeSound();
+std::cout << wild_cat.getType() << std::endl;
+std::cout << wild_dog.getType() << std::endl;
+std::cout << cat.getType() << std::endl;
+std::cout << dog.getType() << std::endl;
+
+std::cout << std::string("+", 25) << std::endl;
+std::cout << "Subject tests" <<std::endl;
+
 const Animal* meta = new Animal();
 const Animal* j = new Dog();
 const Animal* i = new Cat();
