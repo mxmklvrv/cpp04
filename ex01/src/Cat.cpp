abe2637ce4bbc8fb40ca19b1b4fdc1cbf0aec3e5
Maxim Kleverov <mklevero@student.hive.fi>
2026-01-26 19:27:49 +0200

    working on 01

--- ex01/src/Cat.cpp
+++ ex01/src/Cat.cpp
@@ -2,6 +2,12 @@
 
 Cat::Cat() : Animal("Cat"){
 	std::cout << "Cat default constructor called" << std::endl;
+	try{
+		_brain = new Brain();
+	}
+	catch(const std::bad_alloc& e){
+		std::cerr << e.what() << std::endl; // should print std err msg
+	}
 }
 
 Cat::~Cat(){
@@ -10,11 +16,26 @@ Cat::~Cat(){
 
 Cat::Cat(const Cat& other): Animal(other){
 	std::cout << "Cat copy constructor called" << std::endl;
+	try{
+		_brain = new Brain(*other._brain);
+	}
+	catch(const std::bad_alloc& e){
+		std::cerr << e.what() << std::endl;
+	}
+	
 }
 
 Cat& Cat::operator=(const Cat& other){
 	if(this != &other){
 		_type = other._type; 
+		delete this->_brain;
+		try{
+			_brain = new Brain(*other._brain);
+		}
+		catch(const std::bad_alloc& e){
+			std::cerr << e.what() << std::endl;
+			_brain = nullptr;
+		}
 		std::cout << "Cat assignment op called" << std::endl;
 	}
 	return *this;
