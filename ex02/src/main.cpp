de69885689f63032abbcd2ca50f65a4198638003
Maxim Kleverov <mklevero@student.hive.fi>
2026-01-28 16:28:07 +0200

    02 done

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -7,8 +7,13 @@ int main()
 	Animal* sobaka = new Dog();
 	Animal* koshka = new Cat();
 
-	std::cout << "sobaka type" << sobaka->getType() << std::endl;
-	std::cout << "koshka type" << koshka->getType() << std::endl;
+	std::cout << "sobaka type is: " << sobaka->getType() << std::endl;
+	std::cout << "koshka type is: " << koshka->getType() << std::endl;
+
+	sobaka->makeSound();
+	koshka->makeSound();
+
+	//Animal zhivotnoe;
 
 	delete sobaka;
 	delete koshka;
